41b93d834bbf9f6ce27b9614d51d583fdcde5c9c
yezhiguang <847786582@qq.com>
2022-04-18 21:25:19 +0800

    简单的算法提交
    修复了代码格式的错误

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -9,7 +9,6 @@ import org.apache.http.util.EntityUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.util.ArrayList;
